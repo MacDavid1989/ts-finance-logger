4f44ab39df7ae8c3aa3fe25444a6d24b7c9b40bf
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 16:30:12 -0400

    ts will throw an error when passing improper arguements to declared function arguments

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,11 @@
 "use strict";
 var greet;
 // greet = "hello";
+greet = function () {
+    console.log("hey from sandbox");
+};
+var add = function (a, b) {
+    console.log(a + b);
+};
+add(2, 4);
+// add(2, "10");
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,13 @@
 let greet: Function;
 
 // greet = "hello";
+greet = () => {
+  console.log("hey from sandbox");
+};
+
+const add = (a: number, b: number) => {
+  console.log(a + b);
+};
+
+add(2, 4);
+// add(2, "10");
