699356df2801081126963430d427ecff4216c64b
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-25 21:49:41 -0400

    the same applies to arrays of other types as well

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,9 @@
 // arrays
 var names = ["mario", "luigi"];
 names.push("toad");
-names.push(3);
-names[0] = 3;
+// names.push(3);
+// names[0] = 3;
+var numbers = [10, 20, 30];
+// numbers.push('luigi);
+// numbers[0] = 'yoshi;
 // objects
--- sandbox.ts
+++ sandbox.ts
@@ -2,7 +2,11 @@
 let names = ["mario", "luigi"];
 
 names.push("toad");
-names.push(3);
-names[0] = 3;
+// names.push(3);
+// names[0] = 3;
+
+let numbers = [10, 20, 30];
+// numbers.push('luigi);
+// numbers[0] = 'yoshi;
 
 // objects
