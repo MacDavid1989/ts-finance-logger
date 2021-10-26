2475b19b4e39cb7cd6803849a41c68fe6d84e66a
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 17:34:10 -0400

    grab all form elements and typecast accordingly

--- public/app.js
+++ public/app.js
@@ -1,4 +1,12 @@
 "use strict";
-var anchor = document.querySelector("a");
-if (anchor)
-    console.log(anchor.href);
+// const anchor = document.querySelector("a")!;
+// // if (anchor) console.log(anchor.href);
+// console.log(anchor.href);
+// const form = document.querySelector('form')!
+var form = document.querySelector(".new-item-form");
+console.log(form.children);
+// inputs
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,10 @@
 
 // const form = document.querySelector('form')!
 const form = document.querySelector(".new-item-form")! as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector("#type")! as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom")! as HTMLInputElement;
+const details = document.querySelector("#details")! as HTMLInputElement;
+const amount = document.querySelector("#amount")! as HTMLInputElement;
