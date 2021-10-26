c7da8e3e2d0636eca84b881a377b2121f5806b3d
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 17:35:23 -0400

    the callback function argument for event listeners takes in the event object which has a type of Event

--- public/app.js
+++ public/app.js
@@ -4,9 +4,12 @@
 // console.log(anchor.href);
 // const form = document.querySelector('form')!
 var form = document.querySelector(".new-item-form");
-console.log(form.children);
+// console.log(form.children);
 // inputs
 var type = document.querySelector("#type");
 var tofrom = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
