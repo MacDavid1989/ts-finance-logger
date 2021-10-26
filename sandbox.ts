eb559a66768d265274a473249d6f28d126c954b9
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-25 22:16:42 -0400

    the same applies to arrays and objects, however use any reverts ts essentially to js and that's pointless save for very specfic situations

--- sandbox.ts
+++ sandbox.ts
@@ -5,3 +5,17 @@ age = "hello";
 console.log(age);
 age = { name: "luigi" };
 console.log(age);
+
+let mixed: any[] = [];
+
+mixed.push(5);
+mixed.push("mario");
+mixed.push(false);
+
+console.log(mixed);
+
+let ninja: { name: any; age: any };
+ninja = { name: "yoshi", age: 45 };
+console.log(ninja);
+ninja = { name: 45, age: "yoshi" };
+console.log(ninja);
