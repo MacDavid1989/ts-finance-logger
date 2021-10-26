b5ea0484c5e392aff9d36f045f12483f41e3f7d2
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-25 21:57:18 -0400

    if we wanted to declare variables but no assign anything to them we can explicitly declare their type

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,7 @@
+// explicit types
+var character;
+var age;
+var isLoggedIn;
 // arrays
-var names = ["mario", "luigi"];
-names.push("toad");
-// names.push(3);
-// names[0] = 3;
-var numbers = [10, 20, 30];
-// numbers.push('luigi);
-// numbers[0] = 'yoshi;
-var mixed = ["koopa", 3, "bowser", 5];
-mixed.push("peach");
-mixed.push(5);
-mixed[0] = 1;
+// union types
 // objects
-var ninja = {
-    name: "mario",
-    belt: "black",
-    age: 30
-};
-ninja.age = 40;
-ninja.name = "luigi";
-// ninja.age = "30";
-// ninja.skills = ["fighting", "sneaking"];
-ninja = {
-    name: "luigi",
-    belt: "orange"
-};
--- sandbox.ts
+++ sandbox.ts
@@ -1,33 +1,9 @@
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 // arrays
-let names = ["mario", "luigi"];
 
-names.push("toad");
-// names.push(3);
-// names[0] = 3;
-
-let numbers = [10, 20, 30];
-// numbers.push('luigi);
-// numbers[0] = 'yoshi;
-
-let mixed = ["koopa", 3, "bowser", 5];
-mixed.push("peach");
-mixed.push(5);
-mixed[0] = 1;
+// union types
 
 // objects
-let ninja = {
-  name: "mario",
-  belt: "black",
-  age: 30,
-};
-
-ninja.age = 40;
-ninja.name = "luigi";
-// ninja.age = "30";
-// ninja.skills = ["fighting", "sneaking"];
-
-ninja = {
-  name: "luigi",
-  belt: "orange",
-  // age: 40,
-};
