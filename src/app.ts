be759cac86f68a451c66ed033442bc829eb69656
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 19:39:00 -0400

    create an interfaces folder with the file HasFormatter which will be an interface for format() method which ensures it can only return a string

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,3 @@
-// interfaces: enforce structure of class or object
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-
-const me: IsPerson = {
-  name: "shaun",
-  age: 20,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log("I spent", amount);
-    return amount;
-  },
-};
-
-const greetPerson = (person: IsPerson) => {
-  console.log('Hello', person.name)
-};
-
-console.log(me);
-
 import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
