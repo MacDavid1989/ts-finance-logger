6d015122a27fce30968999654902e849ce1ef75e
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 16:46:27 -0400

    we can do the same with objects

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,4 +1,5 @@
 type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
 
 const logDetails = (uid: StringOrNum, item: string) => {
   console.log(`${item} has a uid of ${uid}`);
