b3302a0642da90da8541531c023e20dfd79a8f78
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 16:50:15 -0400

    notice how the greet function follows the signature of the declared greet function

--- public/sandbox.js
+++ public/sandbox.js
@@ -2,5 +2,8 @@
 // let greet: Function
 // eg 1
 var greet;
+greet = function (name, greeting) {
+    console.log(name, greeting);
+};
 // eg 2
 // eg 3
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -3,6 +3,10 @@
 // eg 1
 let greet: (a: string, b: string) => void;
 
+greet = (name: string, greeting: string) => {
+  console.log(name, greeting);
+};
+
 // eg 2
 
 // eg 3
