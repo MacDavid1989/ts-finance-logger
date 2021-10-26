4f3cca53a8099eed0573f796ee3784fc16d347b7
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 17:46:02 -0400

    using ts we can declare that the array of invoices only accepts objects of type Invoice

--- public/app.js
+++ public/app.js
@@ -12,6 +12,8 @@ var Invoice = /** @class */ (function () {
     return Invoice;
 }());
 var invOne = new Invoice("mario", "work on the mario website", 250);
+var invTwo = new Invoice("luigi", "work on the luigi website", 300);
+var invoices = [];
 var form = document.querySelector(".new-item-form");
 // inputs
 var type = document.querySelector("#type");
