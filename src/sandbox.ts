1d25f85f39f94de50862b9ee5ab0e5d999f25ebc
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 16:47:06 -0400

    replace the long object code with the alias to simplify your programming

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,10 +5,10 @@ const logDetails = (uid: StringOrNum, item: string) => {
   console.log(`${item} has a uid of ${uid}`);
 };
 
-const greet = (user: { name: string; uid: StringOrNum }) => {
+const greet = (user: objWithName) => {
   console.log(`${user.name} says hello`);
 };
 
-const greetAgain = (user: { name: string; uid: StringOrNum }) => {
+const greetAgain = (user: objWithName) => {
   console.log(`${user.name} says hello`);
 };
