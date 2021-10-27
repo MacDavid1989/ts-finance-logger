50ce832ce24e2cfb3513277c3d3b611b268ddb83
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 20:33:27 -0400

    enums allow us to associate a set of constants or keywords with a numeric value

--- public/app.js
+++ public/app.js
@@ -1,22 +1,6 @@
 import { Invoice } from "./classes/Invoice.js";
 import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice("yoshi", "web work", 250);
-// docTwo = new Payment("mario", "plumbing work", 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice("mario", "work on the mario website", 250);
-// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
@@ -36,21 +20,14 @@ form.addEventListener("submit", (e) => {
     }
     li.render(doc, type.value, "end");
 });
-// generics: allow us to create reusable blocks of code that can be used with different types
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docOne = addUID({ name: "yoshi", age: 40 });
-console.log(docOne);
 const docTwo = {
     uid: 1,
-    resourceName: "person",
-    data: { name: "shaun" },
+    resourceType: 3,
+    data: { title: "name of the wind" },
 };
 const docThree = {
     uid: 2,
-    resourceName: "shoppingList",
-    data: ["apple", "banana", "orange"],
+    resourceType: 4,
+    data: { name: "yoshi" },
 };
 console.log(docTwo, docThree);
--- src/app.ts
+++ src/app.ts
@@ -3,29 +3,6 @@ import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("yoshi", "web work", 250);
-// docTwo = new Payment("mario", "plumbing work", 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const invOne = new Invoice("mario", "work on the mario website", 250);
-// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
-
 const form = document.querySelector(".new-item-form")! as HTMLFormElement;
 
 // inputs
@@ -51,33 +28,23 @@ form.addEventListener("submit", (e: Event) => {
   li.render(doc, type.value, "end");
 });
 
-// generics: allow us to create reusable blocks of code that can be used with different types
-
-const addUID = <T extends { name: string }>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return { ...obj, uid };
-};
-
-let docOne = addUID({ name: "yoshi", age: 40 });
-console.log(docOne);
-
-// generics with interface
+// enums
 interface Resource<T> {
   uid: number;
-  resourceName: string;
+  resourceType: number;
   data: T;
 }
 
 const docTwo: Resource<object> = {
   uid: 1,
-  resourceName: "person",
-  data: { name: "shaun" },
+  resourceType: 3,
+  data: { title: "name of the wind" },
 };
 
-const docThree: Resource<string[]> = {
+const docThree: Resource<object> = {
   uid: 2,
-  resourceName: "shoppingList",
-  data: ["apple", "banana", "orange"],
+  resourceType: 4,
+  data: { name: "yoshi" },
 };
 
 console.log(docTwo, docThree);
