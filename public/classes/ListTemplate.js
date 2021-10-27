9b16247a3bc16c171be2b273fcbf5e55ac8b1200
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-26 20:06:59 -0400

    create render method to accept item, heading, and position, then for each instance it will create an li the an h4 with innertext heading and a paragraph with innertext item.format() and conditonally check the position it will be added to the container

--- public/classes/ListTemplate.js
+++ public/classes/ListTemplate.js
@@ -1,6 +1,21 @@
 export class ListTemplate {
     constructor(container) {
         this.container = container;
+        render(item, HasFormatter, heading, String, pos, 'start' | 'end');
+        {
+            const li = document.createElement('li');
+            const h4 = document.createElement('h4');
+            h4.innerText = heading;
+            li.append(h4);
+            const p = document.createElement('p');
+            p.innerText = item.format();
+            li.append(p);
+            if (pos === 'start') {
+                this.container.prepend(li);
+            }
+            else {
+                this.container.append(li);
+            }
+        }
     }
-    ;
 }
